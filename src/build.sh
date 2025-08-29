ad71a053bab7ed5b0c94a7c32ecf4ddf5deead98
rishigits <rishigits@users.noreply.github.com>
2025-08-29 13:01:03 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,8 @@ read -ra arr <<< "$@"
 version=${arr[1]}
 trap 0 1 2 ERR
 # Ensure sudo is installed
-apt-get update && apt-get install sudo -y
+apt-get update && apt-get install sudo -
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 bash /tmp/linux-on-ibm-z-scripts/Elasticsearch/${version}/build_elasticsearch.sh -y
 tar cvfz spire-${version}-linux-s390x.tar.gz spire/bin/spire-agent spire/bin/spire-server spire/conf/agent/agent.conf spire/conf/server/server.conf
 cp distribution/archives/linux-s390x-tar/build/distributions/elasticsearch-${version}-SNAPSHOT-linux-s390x.tar.gz elasticsearch-${version}-linux-s390x.tar.gz
