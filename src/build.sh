dc15a226cf4a24b5ec259299a559d0018529bf7f
rishigits <rishigits@users.noreply.github.com>
2025-08-29 13:38:33 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,11 @@ trap 0 1 2 ERR
 apt-get update && apt-get install sudo -
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 bash /tmp/linux-on-ibm-z-scripts/Elasticsearch/${version}/build_elasticsearch.sh -y
-tar cvfz spire-${version}-linux-s390x.tar.gz spire/bin/spire-agent spire/bin/spire-server spire/conf/agent/agent.conf spire/conf/server/server.conf
-cp distribution/archives/linux-s390x-tar/build/distributions/elasticsearch-${version}-SNAPSHOT-linux-s390x.tar.gz elasticsearch-${version}-linux-s390x.tar.gz
+# Build deb, rpm, and docker packages
+cd elasticsearch
+./gradlew :distribution:packages:s390x-deb:assemble -x :libs:entitlement:bridge:compileMain23Java -x :libs:entitlement:compileMain23Java -x :libs:native:compileMain22Java -x :libs:simdvec:compileMain22Java
+./gradlew :distribution:packages:s390x-rpm:assemble -x :libs:entitlement:bridge:compileMain23Java -x :libs:entitlement:compileMain23Java -x :libs:native:compileMain22Java -x :libs:simdvec:compileMain22Java
+./gradlew :distribution:docker:docker-s390x-export:assemble -x :libs:entitlement:bridge:compileMain23Java -x :libs:entitlement:compileMain23Java -x :libs:native:compileMain22Java -x :libs:simdvec:compileMain22Java
+cd ../
+cp elasticsearch/distribution/archives/linux-s390x-tar/build/distributions/elasticsearch-${version}-SNAPSHOT-linux-s390x.tar.gz elasticsearch-${version}-linux-s390x.tar.gz
 exit 0
